4ec9d66474cd7a0d0dd0f00a48d3e378bc4fb165
ashiqulemu.jpi@gmail.com <ashiqulemu.jpi@gmail.com>
2018-11-28 15:31:36 +0600

    ghgh

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -50,7 +50,30 @@
 			</div>
 		</div>
 	</div>
-		.bo
+		
+		<div class="row bottom-footer">
+			<div class="col-md-6">
+				<div class="copyRight">
+					&copy; 2018 Classumo
+				</div>
+			</div>
+			<div class="col-md-6">
+				<ul class="footer-link-items text-right ">
+					<li>
+						<a href="">items </a>
+					</li>
+					<li>
+						<a href="">items </a>
+					</li>
+					<li>
+						<a href="">items </a>
+					</li>
+					<li>
+						<a href="">items </a>
+					</li>
+				</ul>
+			</div>
+		</div>
 	
 	</div>
 </section>
