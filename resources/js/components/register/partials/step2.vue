718ff4da3b4fd4f59c4daad3dc2a8a1a93a82d7f
ashiqulemu.jpi@gmail.com <ashiqulemu.jpi@gmail.com>
2018-11-28 18:53:36 +0600

    comit

--- resources/js/components/register/partials/step2.vue
+++ resources/js/components/register/partials/step2.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		testin2
+	
 	</div>
 </template>
 
--- resources/js/components/register/register.vue
+++ resources/js/components/register/register.vue
@@ -1,7 +1,42 @@
 <template>
 	<div>
-			<step1></step1>
-			<step2></step2>
+			<div class="container">
+				<div class="col-md-2"></div>
+				<div class="col-md-8 mx-auto">
+					<div class="step-main-top">
+						<ul>
+							<li>
+								<div class="step-circle active">
+									<i class="far fa-user"></i>
+								</div>
+								<div>Personal Detail </div>
+							</li>
+							<li>
+								<div class="step-circle">
+									<i class="far fa-newspaper"></i>
+									<!--<img src="/site-images/icons/qualification.PNG" alt="" class="img-responsive">-->
+								</div>
+								<div>Qualification</div>
+							</li>
+							<li>
+								<div class="step-circle">
+									<i class="fas fa-book"></i>
+								</div>
+								<div>Tutoring Subjects</div>
+							</li>
+							<li>
+								<div class="step-circle">
+									<i class="far fa-credit-card"></i>
+								</div>
+								<div>Get Paid</div>
+							</li>
+						</ul>
+					</div>
+					<step1></step1>
+					<step2></step2>
+				</div>
+				<div class="col-md-2"></div>
+			</div>
 	</div>
 </template>
 
