67d1003e91987255334bb630ce22c36061ad55ba
ashiqulemu.jpi@gmail.com <ashiqulemu.jpi@gmail.com>
2018-11-28 14:02:08 +0600

    first commit

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <div class="header">
 	<div class="cover">
 		<div class="container">
-			<nav class="navbar navbar-expand-lg navbar-light">
+			<nav class="navbar navbar-expand-lg navbar-light customNav">
 				<a class="navbar-brand" href="#">LOGO </a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
@@ -14,12 +14,12 @@
 								HOME <span class="sr-only">(current)</span>
 							</a>
 						</li>
-						<li class="nav-item active">
+						<li class="nav-item ">
 							<a class="nav-link" href="#">
 								FIND A TUTOR <span class="sr-only">(current)</span>
 							</a>
 						</li>
-						<li class="nav-item active">
+						<li class="nav-item ">
 							<a class="nav-link" href="#">
 								FIND A TUTOR  <span class="sr-only">(current)</span>
 							</a>
